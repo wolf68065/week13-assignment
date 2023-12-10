5646385c6a51016f38804e4fe8a4a61d9a213530
wolf68065 <145383648+wolf68065@users.noreply.github.com>
2023-12-10 09:58:28 -0600

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,35 @@
+package pet.store.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
+public class Customer {
+
+@Id
+@GeneratedValue
+	private Long customerId;
+	private String customerFirstName;
+	private String customerLastname;
+	
+@Column(unique = true)
+	private String customerEmail;
+
+@EqualsAndHashCode.Exclude
+@ToString.Exclude
+@ManyToMany(mappedBy = "customers", cascade = CascadeType.PERSIST)
+	private Set<PetStore> petStores = new HashSet<>();
+	
+
+}
---
+++ PetStoreApplication.java
@@ -0,0 +1,10 @@
+package pet.stores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class PetStoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApplication.class, args);
+	}
+}
